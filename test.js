a909909d41f18616de9cf616f0f57b5320077698
Alex <alex@theblockchaininstitute.org>
2018-08-01 16:23:47 -0500

    adding updated files for BlockchainWTF login

--- test.js
+++ test.js
@@ -3,14 +3,16 @@ const qrcode = require('qrcode-terminal');
 
 const mnidAddress = '2ov9okPtsptECoKmyu7gt3UR79izc7YbSym';
 const signingKey = '483064dca3d6bbc2a187db48fbf8e20069a3ffaafecafab9f233d0f49f0849b7';
-const appName = 'Blockchain WTF';
+
+const appName = 'The Blockchain Institute';
 
 const uriHandler = (uri) => {
   qrcode.generate(uri, {small: true})
   console.log(uri)
 }
 
-const uport = new uportConnect.Connect(appName, {uriHandler,
+const uport = new uportConnect.Connect(appName, {
+	uriHandler,
     clientId: mnidAddress,
     network: 'rinkeby',
     signer: uportConnect.SimpleSigner(signingKey)
@@ -22,7 +24,7 @@ uport.requestCredentials({
 }).then((credentials) => {
   console.log(credentials);
 
-  
+  console.log('finished')
 
 })
 
